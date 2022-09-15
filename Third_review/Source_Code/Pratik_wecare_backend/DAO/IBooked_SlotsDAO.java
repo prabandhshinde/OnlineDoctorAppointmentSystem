15fbc45042071f4815799eb21485a9c5f96469b7
Pratik Alhat <106821459+alhatpratik@users.noreply.github.com>
2022-09-15 19:56:45 +0530

    Delete Third_review/Source_Code/Pratik_wecare_backend directory

--- Third_review/Source_Code/Pratik_wecare_backend/DAO/IBooked_SlotsDAO.java
+++
@@ -1,9 +0,0 @@
-package com.springrest.springrest.DAO;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springrest.springrest.entities.Booked_Slots;
-
-public interface IBooked_SlotsDAO extends JpaRepository<Booked_Slots, Long> {
-
-}
