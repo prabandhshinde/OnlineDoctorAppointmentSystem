7f9a36fb952e18d1dfd4a55bddfde5ac929c516e
prabandhshinde <prabandh.shd@gmail.com>
2022-09-16 14:29:34 +0530

    patient history JS pages

---
+++ Third_review/Html/ReactUIPages/PatientHistArray.js
@@ -0,0 +1,21 @@
+export default function PatienHistArray(props) {
+  return (
+    <div class="col-lg-6">
+      <div class="member d-flex align-items-start">
+        <div class="pic">
+          <img
+            src="assets/img/doctors/doctors-1.jpg"
+            class="img-fluid"
+            alt=""
+          />
+        </div>
+        <div class="member-info">
+          <h4>{props.name}</h4>
+          <span>{props.spec}</span>
+          <span>{props.date}</span>
+          <span>{props.time}</span>
+        </div>
+      </div>
+    </div>
+  );
+}
