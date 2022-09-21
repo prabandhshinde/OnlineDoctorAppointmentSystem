3d57ce668c0c3b3aa3350a78049177991936b7ea
prabandhshinde <prabandh.shd@gmail.com>
2022-09-21 19:26:45 +0530

    dr appointment history pages

--- Third_review/Html/ReactUIPages/DoctorDailyAppArray.js
+++ Third_review/Html/ReactUIPages/DoctorDailyAppArray.js
@@ -1,14 +1,35 @@
 export default function DoctorDailyAppArray(props) {
   return (
     <>
-      <div class="col-lg-6">
-        <div class="member d-flex align-items-start">
+      <div class="col-lg-12">
+        <div>
           <div class="member-info">
-            <h2>{props.name}</h2>
-            <h5>{props.tm}</h5>
+            <div class="row">
+              <div class="col-lg-2 ">
+                <h4>1)</h4>
+              </div>
+              <div class="col-lg-2">
+                <p>
+                  <h4>{props.tm}</h4>
+                  &ensp;&ensp;&emsp;&emsp;
+                </p>
+              </div>
+              <div class="col-lg-4 ">
+                <h4>{props.name}</h4>
+              </div>
+              <div class="col-lg-4 ">
+                <h4>9689361857</h4>
+              </div>
+            </div>
           </div>
         </div>
       </div>
+      {/* <tr>
+        <td></td>
+        <td>{props.name}</td>
+        <td>{props.tm}</td>
+        <td></td>
+      </tr> */}
     </>
   );
 }
--- Third_review/Html/ReactUIPages/DoctorDailyAppointment.js
+++ Third_review/Html/ReactUIPages/DoctorDailyAppointment.js
@@ -23,21 +23,82 @@ export default function DoctorDailyAppointment() {
               <br />
               <br />
               <br />
-              <h2>Todays Slot</h2>
-              <p>Todays Appointment of Patient are : </p>
+              <h2>Slot</h2>
+              <br />
+              Select Date :{" "}
+              <input
+                type="date"
+                min="09/13/2022"
+                name="start_date"
+                id="end_date"
+                required
+              />
+              <br />
+              <br />
+              <p>
+                Appointment of Patient for -------selected Date-------- are :{" "}
+              </p>
             </div>
             <div class="row">
-              {Appointment.map((eve) => {
-                return (
-                  <DoctorDailyAppArray
-                    key={eve.id}
-                    id={eve.id}
-                    name={eve.name}
-                    tm={eve.tm}
-                  />
-                );
-              })}
+              <center>
+                <div class="col-lg-12">
+                  <div>
+                    <div class="member-info">
+                      <div class="row">
+                        <div class="col-lg-2 ">
+                          <h3>Sr. No.</h3>
+                        </div>
+                        <div class="col-lg-2">
+                          <p>
+                            <h3>Time</h3>
+                            &ensp;&ensp;&emsp;&emsp;
+                          </p>
+                        </div>
+                        <div class="col-lg-4 ">
+                          <h3>Name</h3>
+                        </div>
+                        <div class="col-lg-4 ">
+                          <h3>Contact Number</h3>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {Appointment.map((eve) => {
+                  return (
+                    <DoctorDailyAppArray
+                      key={eve.id}
+                      id={eve.id}
+                      name={eve.name}
+                      tm={eve.tm}
+                    />
+                  );
+                })}
+              </center>
             </div>
+
+            {/* <table border={"2"} str>
+              <thead>
+                <tr>
+                  <td>sr No</td>
+                  <td>Name</td>
+                  <td>time</td>
+                  <td>contact number</td>
+                </tr>
+              </thead>
+              <tbody>
+                {Appointment.map((eve) => {
+                  return (
+                    <DoctorDailyAppArray
+                      key={eve.id}
+                      id={eve.id}
+                      name={eve.name}
+                      tm={eve.tm}
+                    />
+                  );
+                })}
+              </tbody>
+            </table> */}
           </div>
         </section>
       </main>
